dd4c1b5760d2a7a4014cc49adc8a1a36c591e7c0
João V. <joao.vtr.oliveira@outlook.com>
2025-12-05 19:34:24 -0300

    feat: add the services folder with the 'local-storage' service

---
+++ src/app/services/local-storage-service/local-storage-service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+// TODO: Implement the local storage methods to save the battles/sheets in local storage.
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  
+}
