17eca60e4b9be86fec117c8669844e670079a772
Joao V. <joao.vtr.oliveira@outlook.com>
2025-12-06 16:44:07 -0300

    feat: add the hub route in the app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { EncounterBuilder } from './pages/encounter-builder/encounter-builder';
+import { EncounterHub } from './pages/encounter-hub/encounter-hub';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home/encounter-builder', pathMatch: 'full' },
 	{
 		path: 'home',
 		component: Home,
-		children: [{ path: 'encounter-builder', component: EncounterBuilder }],
+		children: [
+			{ path: '', component: EncounterHub },
+			{ path: 'encounter-builder', component: EncounterBuilder },
+			{ path: 'encounter-builder/:id', component: EncounterBuilder },
+		],
 	},
 ];
