8d7554d33e5926c9557a1dde5f9769310af1baae
Joao V. <joao.vtr.oliveira@outlook.com>
2025-12-08 12:30:38 -0300

    feat: add the new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { EncounterBuilder } from './pages/encounter-builder/encounter-builder';
 import { EncounterHub } from './pages/encounter-hub/encounter-hub';
+import { HomebrewSheets } from './pages/homebrew-sheets/homebrew-sheets';
+import { Calendar } from './pages/calendar/calendar';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'home/encounter-builder', pathMatch: 'full' },
@@ -12,6 +14,10 @@ export const routes: Routes = [
 			{ path: '', component: EncounterHub },
 			{ path: 'encounter-builder', component: EncounterBuilder },
 			{ path: 'encounter-builder/:id', component: EncounterBuilder },
+			{ path: 'homebrew', component: HomebrewSheets },
+			{ path: 'calendar', component: Calendar },
 		],
 	},
+
+	{ path: '**', redirectTo: 'home' },
 ];
