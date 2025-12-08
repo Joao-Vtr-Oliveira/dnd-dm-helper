3ba20583c387a55951ac0a3f1734814968e3304b
Joao V. <joao.vtr.oliveira@outlook.com>
2025-12-08 12:30:45 -0300

    feat: add the sidenav

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,15 +1,41 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+
+type NavLink = { label: string; icon: string; path: string; exact?: boolean };
 
 @Component({
 	selector: 'app-home',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, RouterModule],
 	templateUrl: './home.html',
 })
 export class Home {
 	private router = inject(Router);
 
-	onClickTitle () {
-		this.router.navigate(['/home'])
+	onClickTitle() {
+		this.router.navigate(['/home']);
 	}
+
+	navLinks: NavLink[] = [
+		{
+			label: 'Encounter Hub',
+			icon: '/svgs/home.svg',
+			path: '/home',
+			exact: true,
+		},
+		{
+			label: 'Encounter Builder',
+			icon: '/svgs/sword.svg',
+			path: '/home/encounter-builder',
+		},
+		{
+			label: 'Homebrew Sheets',
+			icon: '/svgs/sheet.svg',
+			path: '/home/homebrew',
+		},
+		{
+			label: 'Calendar',
+			icon: '/svgs/calendar.svg',
+			path: '/home/calendar',
+		},
+	];
 }
